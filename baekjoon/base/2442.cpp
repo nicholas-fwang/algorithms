f6f6adba9b3e42d3f03943982c5478a31be4ddaa
fisache <hwang031451@gamil.com>
2016-10-19 15:09:06 +0900

    boj 2442

---
+++ baekjoon/base/2442.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);  
+    for (int i=n-1; i>=0; i--) {
+		for (int j=0; j<(n*2)-1-i; j++) {
+			if (i > j) printf(" ");
+			else printf("*");
+		}
+		printf("\n");
+	}
+    return 0;
+}
