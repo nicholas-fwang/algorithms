a2c4226a957588409ee4d99a91a28232cbc85aaf
fisache <hwang031451@gamil.com>
2016-10-19 14:57:29 +0900

    boj 5338

---
+++ baekjoon/base/5338.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    cout << "       _.-;;-._" << endl;
+    cout << "'-..-'|   ||   |"<< endl;
+    cout << "'-..-'|_.-;;-._|"<< endl;
+    cout << "'-..-'|   ||   |"<< endl;
+    cout << "'-..-'|_.-''-._|"<< endl;
+    return 0;
+}
