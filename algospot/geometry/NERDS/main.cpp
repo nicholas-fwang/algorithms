87247271f0d6b33328b2ac9aa8c46ac0ec70b79e
fisache <hwang031451@gmail.com>
2017-09-18 23:39:04 +0900

    algospot NERD

--- algospot/geometry/NERDS/main.cpp
+++ algospot/geometry/NERDS/main.cpp
@@ -1,176 +1,157 @@
 #include <iostream>
-#include <cmath>
+#include <cstdio>
 #include <vector>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 struct vector2 {
-    double x, y;
-    explicit vector2(double x_=0, double y_=0) : x(x_), y(y_) {
-        
-    }
-    
-    bool operator < (const vector2& rhs) const {
-        return x == rhs.x ? y < rhs.y : x < rhs.x;
-    }
-    
-    vector2 operator + (const vector2& rhs) const {
-        return vector2(x+rhs.x, y+rhs.y);
-    }
-    
-    vector2 operator - (const vector2& rhs) const {
-        return vector2(x-rhs.x, y-rhs.y);
-    }
-    
-    vector2 operator * (double rhs) const {
-        return vector2(rhs*x, rhs*y);
-    }
-    
-    double cross(const vector2& rhs) const {
-        return x*rhs.y - y*rhs.x;
-    }
-    
-    bool isEqual(const vector2& rhs) const {
-        return x == rhs.x && y == rhs.y;
-    }
-    
-    double norm() const {
-        return hypot(x, y);
-    }
+  int x,y;
+
+  vector2(int _x=0, int _y=0) : x(_x), y(_y) {}
+
+  bool operator == (const vector2& rhs) const {
+    return x == rhs.x && y == rhs.y;
+  }
+
+  bool operator < (const vector2& rhs) const {
+    return x != rhs.x ? x < rhs.x : y < rhs.y;
+  }
+
+  vector2 operator + (const vector2& rhs) const {
+    return vector2(x+rhs.x, y+rhs.y);
+  }
+
+  vector2 operator - (const vector2& rhs) const {
+    return vector2(x-rhs.x, y-rhs.y);
+  }
+
+  vector2 operator * (int rhs) const {
+    return vector2(rhs*x, rhs*y);
+  }
+
+  double norm() const {
+    return hypot(x,y);
+  }
+
+  vector2 normalize() const {
+    return vector2(x/norm(), y/norm());
+  }
+
+  double dot(const vector2& rhs) const {
+      return x * rhs.x + y * rhs.y;
+  }
+
+  double cross(const vector2& rhs) const {
+      return x * rhs.y - rhs.x * y;
+  }
 };
 
-vector2 getMostLeft(vector<vector2>& poly);
-vector2 getNextEdge(vector<vector2>& poly, vector2& edge);
-double ccw(vector2& edge, vector2& a, vector2& b);
-bool isInside(vector2& edge, vector<vector2>& poly);
-bool polygonIntersects(vector<vector2>& p, vector<vector2>& q);
+double ccw(vector2 a, vector2 b) {
+  return a.cross(b);
+}
+
+double ccw(vector2 p, vector2 a, vector2 b) {
+  return ccw(a-p, b-p);
+}
+
+bool segmentIntersects(vector2 a, vector2 b, vector2 c, vector2 d) {
+  double ab = ccw(a, b, c) * ccw(a, b, d);
+  double cd = ccw(c, d, a) * ccw(c, d, b);
+
+  if(ab == 0 && cd == 0) {
+    if(b < a) swap(a,b);
+    if(d < c) swap(c,d);
+
+    return !((b < c) || (d < a));
+  }
+
+  return ab <= 0 && cd <= 0;
+}
+
+typedef vector<vector2> polygon;
+
+polygon wrapGift(vector<vector2>& points) {
+  int n = points.size();
+  polygon ret;
+  vector2 p = *min_element(points.begin(), points.end());
+  ret.push_back(p);
+  while(true) {
+    vector2 ph = ret.back();
+    vector2 next = points[0];
+    for(int i=1; i<n; i++) {
+      double cross = ccw(ph, next, points[i]);
+      double dist = (next-ph).norm() - (points[i]-ph).norm();
+      if(cross > 0 || (cross == 0 && dist < 0)) {
+        next = points[i];
+      }
+    }
+    if(next == p) break;
+    ret.push_back(next);
+  }
+  return ret;
+}
+
+bool isInside(vector2 q, const vector<vector2>& p)
+{
+	int crosses = 0;
+	for(int i=0; i < p.size(); i++)
+	{
+		int j = (i + 1) % p.size();
+
+		if((p[i].y > q.y) != (p[j].y > q.y))
+		{
+			double atX = (p[j].x - p[i].x) * (q.y - p[i].y) / (p[j].y - p[i].y) + p[i].x;
+			if(q.x < atX)
+				crosses++;
+		}
+	}
+	return crosses % 2 > 0;
+}
+
+bool polygonIntersects(polygon& p1, polygon& p2) {
+  int n1 = p1.size();
+  int n2 = p2.size();
+
+  if(isInside(p1[0], p2) || isInside(p2[0], p1)) return true;
+
+  for(int i=0; i<n1; i++) {
+    for(int j=0; j<n2; j++) {
+      vector2 a = p1[i];
+      vector2 b = p1[(i+1)%n1];
+      vector2 c = p2[j];
+      vector2 d = p2[(j+1)%n2];
+      if(segmentIntersects(a,b,c,d)) return true;
+    }
+  }
+  return false;
+}
 
 int main()
 {
-    int testCase;
-    cin >> testCase;
-    while(testCase--) {
-        vector<vector2> nerds;
-        vector<vector2> notNerds;
-        vector<vector2> nerdsPolygon;
-        vector<vector2> nNerdsPolygon;
-        int num;
-        bool result;
-        cin >> num;
-        for(int i=0; i<num; i++) {
-            int n, s, t;
-            cin >> n >> s >> t;
-            if(n == 1) nerds.push_back(vector2(s, t));
-            else notNerds.push_back(vector2(s, t));
-        }
-        // nerd 선물포장
-        // 가장 왼쪽점을 찾는다
-        vector2 mostLeft = getMostLeft(nerds);
-        nerdsPolygon.push_back(mostLeft);
-        
-         // edge들을 찾는다
-        vector2 edge = getNextEdge(nerds, mostLeft);
-        while(!edge.isEqual(mostLeft)) {
-            nerdsPolygon.push_back(edge);
-            edge = getNextEdge(nerds, nerdsPolygon.back());
-        }
-        
-        // notNerd 선물포장
-        vector2 nMostLeft = getMostLeft(notNerds);
-        nNerdsPolygon.push_back(nMostLeft);
-        
-        vector2 nEdge = getNextEdge(notNerds, nMostLeft);
-        while(!nEdge.isEqual(nMostLeft)) {
-            nNerdsPolygon.push_back(nEdge);
-            nEdge = getNextEdge(notNerds, nNerdsPolygon.back());
-        }
-        
-        // 두 다각형이 겹치는지 확인한다
-        result = polygonIntersects(nerdsPolygon, nNerdsPolygon);
-        
-        if(!result) cout << "THEORY HOLDS" << endl;
-        else cout << "THEORY IS INVALID" << endl;
+  int testCase;
+  scanf("%d", &testCase);
+  for(int tc=1; tc<=testCase; tc++) {
+    int N;
+    vector<vector2> p1,p2;
+    scanf("%d", &N);
+    for(int i=0; i<N; i++) {
+      int type, x, y;
+      scanf("%d %d %d", &type, &x, &y);
+      if(type == 0) {
+        p1.push_back(vector2(x,y));
+      } else {
+        p2.push_back(vector2(x,y));
+      }
     }
-    return 0;
-}
-
-vector2 getMostLeft(vector<vector2>& poly)
-{
-    vector2 mostLeft = vector2(poly[0].x, poly[0].y);
-    for(int i=1; i<poly.size(); i++) {
-        if(mostLeft.x > poly[i].x) {
-            mostLeft = poly[i];
-        }
+    polygon a = wrapGift(p1);
+    polygon b = wrapGift(p2);
+    if(polygonIntersects(a,b)) {
+      printf("THEORY IS INVALID\n");
+    } else {
+      printf("THEORY HOLDS\n");
     }
-    return mostLeft;
-}
-
-double ccw(vector2& edge, vector2& a, vector2& b)
-{
-    // b-edge가 왼쪽에 있으면 양수
-    return (a-edge).cross(b-edge);;
-}
-
-// 선물 포장을 위해 edge에 가장 왼쪽에 있는 다음 지점을 찾는다
-vector2 getNextEdge(vector<vector2>& poly, vector2& edge)
-{
-    vector2 nextEdge = poly[0];
-    for(int i=1; i<poly.size(); i++) {
-        double r = ccw(edge, nextEdge, poly[i]);
-        double dist = (nextEdge - edge).norm() - (poly[i] - edge).norm();
-        if(r > 0 || (r == 0 && dist < 0)) {
-            nextEdge = poly[i];
-        }
-    }
-    return nextEdge;
-}
-
-// poly 다각형 안에 edge가 있는지 확인한다
-bool isInside(vector2& edge, vector<vector2>& poly)
-{
-    int crosses = 0;
-    for(int i=0; i<poly.size(); i++) {
-        int j = (i + 1) % poly.size();
-        if((poly[i].y > edge.y) != (poly[j].y > edge.y)) {
-            double atX = (poly[j].x - poly[i].x) * (edge.y - poly[i].y) / (poly[j].y - poly[i].y) + poly[i].x;
-            if(edge.x < atX) {
-                crosses++;
-            }
-        }
-    }
-    return crosses % 2 > 0;
-}
-
-// 선분 ab와 선분 cb가 겹치는지 확인한다
-bool segmentIntersects(vector2 a, vector2 b,
-                      vector2 c, vector2 d) {
-    double ab = ccw(a, b, c) * ccw(a, b, d);
-    double cd = ccw(c, d, a) * ccw(c, d, b);
-    
-    if(ab == 0 && cd == 0) {
-        if(b < a) {
-            swap(a, b);
-        }
-        if(d < c) {
-            swap(c, d);
-        }
-        return !(b < c || d < a);
-    }
-    return ab <= 0 && cd <= 0;
-}
-
-// 두 다각형이 겹치는지 확인한다
-bool polygonIntersects(vector<vector2>& p, vector<vector2>& q)
-{
-    int n = p.size(), m = q.size();
-    if(isInside(p[0], q) || isInside(q[0], p)) return true;
-    
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
-            if(segmentIntersects(p[i], p[(i+1)%n], q[j], q[(j+1)%m])) {
-                return true;
-            }
-        }
-    }
-    return false;
+  }
+  return 0;
 }
