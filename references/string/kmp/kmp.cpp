33ec0456fc14b2d99993ba7908a9fc7c4856d4aa
fisache <hwang031451@gmail.com>
2017-10-29 13:28:16 +0900

    kmp references

---
+++ references/string/kmp/kmp.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<int> getpi(string N)
+{
+  int n = N.size();
+  vector<int> pi(n, 0);
+  int j=0;
+  for(int i=1; i<n; i++) {
+    while(j > 0 && N[i] != N[j]) {
+      j = pi[j-1];
+    }
+    if(N[i] == N[j]) {
+      pi[i] = ++j;
+    }
+  }
+}
+
+vector<int> kmpSearch(string H, string N)
+{
+  int h = H.size();
+  int n = N.size();
+
+  vector<int> ret;
+
+  int j=0;
+  for(int i=1; i<h; i++) {
+    while(j > 0 && H[i] != N[j]) {
+      j = pi[j-1];
+    }
+    if(H[i] == N[j]) {
+      if(j == n-1) {
+        ret.push_back(i-j+1);
+        j = pi[j];
+      }
+      else {
+        j++;
+      }
+    }
+  }
+}
