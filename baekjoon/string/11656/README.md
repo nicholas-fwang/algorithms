3bbccdd268d705509ff3a07a290a930b71e98402
fisache <hwang031451@gmail.com>
2017-11-26 09:28:49 +0900

    boj 11656

---
+++ baekjoon/string/11656/README.md
@@ -0,0 +1,15 @@
+link :
+https://www.acmicpc.net/problem/11656
+
+input example :
+baekjoon
+
+output example :
+aekjoon
+baekjoon
+ekjoon
+joon
+kjoon
+n
+on
+oon
---
+++ baekjoon/string/11656/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Comparator {
+  const vector<int>& group;
+  int t;
+  Comparator(const vector<int>& _group, int _t): group(_group), t(_t) {
+  }
+
+  bool operator () (int a, int b) {
+    if(group[a] != group[b]) return group[a] < group[b];
+    return group[a+t] < group[b+t];
+  }
+};
+
+vector<int> getSA(const string& s) {
+  int n = s.size();
+
+  int t = 1;
+  vector<int> group(n+1);
+  for(int i=0; i<n; i++) group[i] = s[i];
+  group[n] = -1;
+  vector<int> perm(n);
+  for(int i=0; i<n; i++) perm[i] = i;
+  while(t < n) {
+    Comparator compareUsing2T(group, t);
+    sort(perm.begin(), perm.end(), compareUsing2T);
+    t *= 2;
+    if(t >= n) break;
+    vector<int> newGroup(n+1);
+    newGroup[n] = -1;
+    newGroup[perm[0]] = 0;
+    for(int i=1; i<n; i++) {
+      if(compareUsing2T(perm[i-1], perm[i])) {
+        newGroup[perm[i]] = newGroup[perm[i-1]] + 1;
+      }
+      else {
+        newGroup[perm[i]] = newGroup[perm[i-1]];
+      }
+    }
+    group = newGroup;
+  }
+  return perm;
+}
+
+int main()
+{
+  string s;
+  cin >> s;
+  vector<int> ret = getSA(s);
+  for(int i=0; i<ret.size(); i++) {
+    cout << s.substr(ret[i]) << "\n";
+  }
+  return 0;
+}
